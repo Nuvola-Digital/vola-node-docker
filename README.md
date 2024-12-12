5420a40791e6b215f02b70a2468eb9f5c332c5c7
rupeshgyawali <rupesh@ngit.com.np>
2024-12-12 16:42:37 +0545

    feat: docker compose to run private testnet node

---
+++ README.md
@@ -0,0 +1,13 @@
+# Vola Node Docker
+
+This repository contains scripts for easy orchestration of the Vola Node.
+
+## Requirement
+
+- Docker Compose
+
+## How to run?
+
+- Clone repo
+- Copy .env.example to .env and update .env file.
+- Run `docker compose up`
---
+++ run-private-testnet-node.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Add Aura key to keystore
+echo "Inserting Aura key into keystore..."
+/usr/local/bin/vola-node key insert --chain private-testnet \
+    --scheme Sr25519 --suri "$AURA_SURI" --key-type aura --password "$KEYSTORE_PASSWORD"
+
+# Add Grandpa key to keystore
+echo "Inserting Grandpa key into keystore..."
+/usr/local/bin/vola-node key insert --chain private-testnet \
+    --scheme Ed25519 --suri "$AURA_SURI" --key-type gran --password "$KEYSTORE_PASSWORD"
+
+# Run the node
+echo "Starting the node..."
+exec /usr/local/bin/vola-node --validator --chain private-testnet --port "$PORT" --rpc-port "$RPC_PORT" \
+    --rpc-methods safe --name "$NODE_NAME" --no-telemetry --no-prometheus --node-key "$NODE_KEY"
